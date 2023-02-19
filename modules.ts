01ad2cc6b6bab99c718d2e47fac1f59f68cf5d37
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-19 12:36:00 -0500

    convert modules to record

--- modules.ts
+++ modules.ts
@@ -1,25 +1,26 @@
 import type { PermissionFlagsBits } from "discord.js";
 
 export default {
-    modules: [
-        { id: "modules", name: "Manage Modules", commands: ["modules"] },
-        { id: "admin", name: "Admin Controls", commands: ["admin"] },
-        {
-            id: "moderation",
+    modules: {
+        modules: { name: "Manage Modules", commands: ["modules"] },
+        admin: { name: "Admin Controls", commands: ["admin"] },
+        moderation: {
             name: "Moderation",
             icon: "shield",
             description:
                 "Moderation features for your moderators. Automated tools are in other modules such as the automod module.",
             commands: ["ban"],
         },
-    ] as {
-        id: string;
-        name: string;
-        icon?: string;
-        description?: string;
-        commands: string[];
-        disable?: boolean;
-    }[],
+    } as Record<
+        string,
+        {
+            name: string;
+            icon?: string;
+            description?: string;
+            commands: string[];
+            disable?: boolean;
+        }
+    >,
     commands: {
         modules: { name: "Manage Modules", permissions: "admin" },
         admin: { name: "Admin Controls", permissions: "admin" },
