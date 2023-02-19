3e5ff89946b44dd15315865cda8bc1043636ae4c
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-18 23:37:51 -0500

    add some data for testing

---
+++ modules.ts
@@ -0,0 +1,39 @@
+import { PermissionFlagsBits, PermissionResolvable } from "discord.js";
+
+export default {
+    modules: [
+        { name: "modules", commands: ["modules"] },
+        { name: "admin", commands: ["admin"] },
+        {
+            name: "moderation",
+            description:
+                "Moderation features for your moderators. Automated tools are in other modules such as the automod module.",
+            commands: ["ban"],
+        },
+    ] satisfies {
+        name: string;
+        commands: string[];
+        description?: string;
+    }[],
+    commands: {
+        modules: { permissions: "admin" },
+        admin: { permissions: "admin" },
+        "admin/promote": { permissions: "owner" },
+        "admin/demote": { permissions: "owner" },
+        "admin/eval": { permissions: "owner" },
+        ban: {
+            description:
+                "Ban a user from the server, even if they are not in the server.",
+            permissions:
+                PermissionFlagsBits.BanMembers |
+                PermissionFlagsBits.ManageChannels,
+        },
+    } satisfies Record<
+        string,
+        {
+            permissions?: string | PermissionResolvable;
+            description?: string;
+            bypass?: boolean;
+        }
+    >,
+};
