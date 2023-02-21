2661043f364f24311df68790880ec63e1964b80a
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-21 03:18:49 -0500

    add core

--- modules.ts
+++ modules.ts
@@ -4,12 +4,11 @@ export default {
     modules: {
         modules: { name: "Manage Modules", commands: ["modules"] },
         admin: { name: "Admin Controls", commands: ["admin"] },
-        moderation: {
-            name: "Moderation",
-            icon: "shield",
-            description:
-                "Moderation features for your moderators. Automated tools are in other modules such as the automod module.",
-            commands: ["ban"],
+        core: {
+            name: "Core",
+            icon: "label_important_outline",
+            description: "Core commands for managing and configuring the bot.",
+            commands: ["settings"],
         },
     } as Record<
         string,
@@ -24,12 +23,11 @@ export default {
     commands: {
         modules: { name: "Manage Modules", permissions: "admin", ghost: true },
         admin: { name: "Admin Controls", permissions: "admin", ghost: true },
-        ban: {
-            name: "Ban User",
-            icon: "gavel",
-            description: "Ban a user from the server, even if they are not in the server.",
-            permissions: "BanMembers",
-            self_permissions: "BanMembers",
+        settings: {
+            name: "Settings",
+            icon: "settings",
+            permissions: "ManageGuild",
+            description: "Manage bot settings.",
         },
     } as Record<
         string,
