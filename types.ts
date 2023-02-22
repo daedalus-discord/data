5945846365745f4752a5f74bcb2db530bc343949
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-22 10:38:03 -0500

    extract type

--- types.ts
+++ types.ts
@@ -2,6 +2,28 @@ import type { Channel, Guild, GuildMember, Message, Role, User } from "discord.j
 
 export type CustomMessageComponent = (string | number | boolean | CustomMessageComponent)[];
 export type Interpolation = (string | CustomMessageComponent)[];
+export type CustomMessage = {
+    content?: Interpolation;
+    embeds?: {
+        title?: Interpolation;
+        description?: Interpolation;
+        url?: Interpolation;
+        color?: number;
+        footer?: { text: Interpolation; icon_url?: Interpolation };
+        image?: { url: Interpolation };
+        thumbnail?: { url: Interpolation };
+        author?: {
+            name: Interpolation;
+            url?: Interpolation;
+            icon_url?: Interpolation;
+        };
+        fields?: {
+            name: Interpolation;
+            value: Interpolation;
+            inline?: boolean;
+        }[];
+    }[];
+};
 export type userdata = string | number | boolean | userdata[];
 export type ContextData = {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
