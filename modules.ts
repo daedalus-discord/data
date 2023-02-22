a931903e2b92156e53242a747fb15b6aa5033dbe
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-22 18:34:46 -0500

    add xp

--- modules.ts
+++ modules.ts
@@ -22,6 +22,12 @@ export default {
             description: "Announce new boosters and other server supporters by role.",
             commands: [],
         },
+        xp: {
+            name: "XP",
+            icon: "sort",
+            description: "Server experience system to reward active members.",
+            commands: ["top", "rank", "mee6-import", "reset-xp"],
+        },
     } as Record<
         string,
         {
@@ -41,6 +47,28 @@ export default {
             permissions: "ManageGuild",
             self_permissions: "ManageWebhooks",
             description: "Manage bot settings.",
+        },
+        top: {
+            name: "Top",
+            icon: "leaderboard",
+            description: "Get top users by XP.",
+        },
+        rank: {
+            name: "Rank",
+            icon: "military_tech",
+            description: "Get a user's XP and rank.",
+        },
+        "mee6-import": {
+            name: "MEE6 XP Import",
+            icon: "import_export",
+            permissions: "ManageGuild",
+            description: "Import XP from MEE6",
+        },
+        "reset-xp": {
+            name: "Reset XP",
+            icon: "restart_alt",
+            permissions: "ManageGuild",
+            description: "Reset a user's XP irreversibly.",
         },
     } as Record<
         string,
